2eea724447207667b4d6e5d65ea05d946569c34e
MudasirHussain72 <mudasirhussain41302@gmail.com>
2023-01-25 21:55:56 +0500

    Utils added

---
+++ lib/utils/utils.dart
@@ -0,0 +1,31 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:another_flushbar/flushbar_route.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class Utils {
+  static toastMessage(String message) {
+    Fluttertoast.showToast(msg: message);
+  }
+
+  static void flushBarErrorMessage(String message, BuildContext, context) {
+    showFlushbar(
+        context: context,
+        flushbar: Flushbar(
+          message: message,
+          forwardAnimationCurve: Curves.easeIn,
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          padding: EdgeInsets.all(15),
+          duration: Duration(seconds: 3),
+          borderRadius: BorderRadius.circular(10),
+          backgroundColor: Colors.red.shade400,
+          positionOffset: 20,
+          icon: Icon(
+            Icons.error,
+            size: 20,
+            color: Colors.white,
+          ),
+        )..show(context));
+  }
+}
--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm/utils/routes/routes_name.dart';
+import 'package:mvvm/utils/utils.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -14,7 +15,11 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
         body: Center(
       child: InkWell(
-          onTap: () => Navigator.pushNamed(context, RoutesName.home),
+          // onTap: () => Navigator.pushNamed(context, RoutesName.home),
+          onTap: () {
+            Utils.flushBarErrorMessage("message", BuildContext,
+                context); // Utils.toastMessage("you clicked on go to home screen");
+          },
           child: Text("go to home screen")),
     ));
   }
